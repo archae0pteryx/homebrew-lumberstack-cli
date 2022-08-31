580c324cd6e496ded38209c0294f7d8f5c08083f
Ryan <thefakeryan@icloud.com>
2022-08-31 12:33:08 -0700

    update homepage

--- Formula/lumberstack-cli.rb
+++ Formula/lumberstack-cli.rb
@@ -1,6 +1,6 @@
 class LumberstackCli < Formula
   desc "Configurable project generator with a redwoodjs core"
-  homepage ""
+  homepage "https://github.com/archae0pteryx/lumberstack-cli"
   url "https://github.com/archae0pteryx/lumberstack-cli/releases/download/v0.1.2-beta.1/lumberstack-cli_v0.1.2-beta.1_x86_64-apple-darwin.tar.gz"
   sha256 "2b742d9a8600300daad233b968a2d3fb252f1aaf41405db0f6a41378491ec85c"
   version "v0.1.2-beta.1"
